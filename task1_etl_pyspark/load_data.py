e3afc3479288c436a211a6fc758c0f640851cd9a
nverbies <verbiestnele@gmail.com>
2022-02-10 16:22:57 +0000

    Added last task

--- task1_etl_pyspark/load_data.py
+++ task1_etl_pyspark/load_data.py
@@ -4,10 +4,11 @@ import configparser
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 from pyspark.sql import functions as f
+import json 
 
 my_bucket = 's3a://dataminded-academy-capstone-resources/raw/open_aq/'
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.hadoop:hadoop-aws:3.1.2 pyspark-shell"
+#os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.hadoop:hadoop-aws:3.1.2 pyspark-shell"
 
 spark_conf= SparkConf()
 spark_conf.set("spark.jars.packages","org.apache.hadoop-aws:3.1.2,net.snowflake:spark-snowflake_2.12:2.9.0-spark_3.1,net.snowflake:snowflake-jdbc:3.13.3")
@@ -31,9 +32,19 @@ print(df.dtypes)
 secret = 'snowflake/capstone/login'
 session = boto3.session.Session()
 s3_client = session.client('secretsmanager')
-key = s3_client.get_secret_value(SecretId=secret)
-print(key)
+retrieved_secret = s3_client.get_secret_value(SecretId=secret)["SecretString"]
 
+retrieved_secret = json.loads(retrieved_secret)
 
-# Load to snowflake
+sfOptions = {
+    "sfURL":retrieved_secret['URL']+".snowflakecomputing.com",
+    "sfWarehouse": retrieved_secret['WAREHOUSE'],
+    "sfUser": retrieved_secret['USER_NAME'],
+    "sfRole": retrieved_secret['ROLE'],
+    "sfPassword": retrieved_secret['PASSWORD'],
+    "sfDatabase": retrieved_secret['DATABASE'],
+    "sfSchema": 'NELE'
+}
 
+SNOWFLAKE_SOURCE_NAME = "net.snowflake.spark.snowflake"
+df.write.format(SNOWFLAKE_SOURCE_NAME).options(**sfOptions).option("dbtable","airquality").mode("overwrite").save()
